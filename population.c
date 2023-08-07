55522f7f1d9d2fdc515e001c180934959e345106
Jinx606 <108185374+Jinx606@users.noreply.github.com>
2023-08-07 10:42:09 +0200

    Add files via upload

---
+++ population.c
@@ -0,0 +1,58 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+
+
+int main(void)
+{
+
+
+
+    // TODO: Prompt for start size
+    int start;
+    {
+        do
+        {
+           start = get_int("start size: ");
+        }
+        while (start < 9);
+
+
+
+    // TODO: Prompt for end size
+    int end;
+    {
+        do
+        {
+            end = get_int("end size: ");
+        }
+        while (end < start);
+
+    }
+
+    // TODO: Calculate number of years until we reach threshold
+    int n = start;
+    int years = 0;
+
+    while (n < end)
+    {
+        n = n + n / 3 - n / 4;
+        years++;
+    }
+
+
+    // TODO: Print number of years
+    printf("Years: %i\n", years);
+
+
+    }
+}
+
+
+
+
+
+
+
+
